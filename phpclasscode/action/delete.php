f662ffc3af9b358056e81646bbf21fd003d6de96
asadmukhtarr <axad03213@gmail.com>
2023-03-11 19:49:35 +0500

    error

---
+++ phpclasscode/action/delete.php
@@ -0,0 +1,8 @@
+<?php 
+    $id = $_GET['id'];
+    // connectoins
+    include('cn.php');
+    $query = "DELETE FROM `users` WHERE id='$id'";
+    mysqli_query($cn,$query) or die('cant run query');
+    header("location:../home.php?delete='User Deleted Successfuly'");
+?>
